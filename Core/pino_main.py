49a765bb6dbc910474d301253920c4b8149b8c5d
jiwoo pi-zero-w <jiwoo@gepetto.io>
2020-03-23 06:56:25 +0000

    add Cloud object and check works fine

--- Core/pino_main.py
+++ Core/pino_main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3.7
 
 from Hardware.v1 import HardwareV1
+from Cloud.Google import pino_dialogflow
 
 class Pinobot():
     def __init__(self):
         self.HardWare = HardwareV1()
         self.HardWare.write(text="init hardware v1")
+
+        DIALOGFLOW_PROJECT_ID = 'a2-bwogyf'
+        DIALOGFLOW_LANGUAGE_CODE = 'ko'
+        GOOGLE_APPLICATION_CREDENTIALS = '/home/pi/PinoBot/Keys/a2-bwogyf-c40e46d0dc2b.json'
+
+        self.Cloud = pino_dialogflow.PinoDialogFlow(DIALOGFLOW_PROJECT_ID,
+                                                    DIALOGFLOW_LANGUAGE_CODE,
+                                                    GOOGLE_APPLICATION_CREDENTIALS)
         pass
     
     def boot(self):
