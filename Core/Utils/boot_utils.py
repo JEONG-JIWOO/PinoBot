ced66ddcc84110ed4e28de4726f53bf5c6caae55
JEONG-JIWOO <jjw951215@gmail.com>
2020-05-12 16:24:16 +0900

    1. add LED change, 2. check bootutile works.
    TODO
    1. use config file
    2. refactoring

--- Core/Utils/boot_utils.py
+++ Core/Utils/boot_utils.py
@@ -44,6 +44,7 @@ class BootLoader():
                 print("[E21] wpa_supp1licant.conf error")
                 self.hardware.write_text_line1(text="BOOT-ERROR")
                 self.hardware.write_text_line2(text="E21 WIFI-SET-WRONG")
+                self.hardware.write(led=[255, 0, 0]) # RED LED on
                 return -1
             else :
                 # 3.1.2, wpa_supplicant.conf is fine  re-set wifi
@@ -52,11 +53,17 @@ class BootLoader():
                     subprocess.check_output('sudo sh '+self.base_path+'/Core/Utils/wifiReset.sh', shell=True).decode('utf-8')
                     print("reset wifi")
                     self.hardware.write_text_line1(text="WIFI RE-CONNECT")
+                    self.hardware.write(led=[205, 140, 0])  # Orange LED on
                     cnt = 0
                     for i in tqdm.trange(15):
                         time.sleep(2)
                         cnt += 1
                         self.hardware.write_text_line2(text="=" * cnt)
+                        if cnt % 2 ==0:
+                            self.hardware.write(led=[205, 140, 0]) # Orange LED ON
+                        else :
+                            self.hardware.write(led=[30, 140, 0]) # GREEN LED ON
+
                 except:
                     response = requests.get('https://status.cloud.google.com/', timeout=2.50)
                     if response.status_code != 200:  # if internet ok.
@@ -65,11 +72,13 @@ class BootLoader():
                     print("[E22] wifi name, password wrong")
                     self.hardware.write_text_line1(text="WIFI-ERROR")
                     self.hardware.write_text_line2(text="E22 NO-WIFI")
+                    self.hardware.write(led=[255, 0, 100]) # PURPLE LED ON
                     return -1
 
         #  3.2. Network is connected
         print("Network OK!")
-        self.hardware.write(text="Network OK!")
+        self.hardware.write(text="Network OK!", led=[60, 60, 150], servo=[20, 70, 40])
+        time.sleep(1)
 
         # 4. load Configure file
         self.load_config()
@@ -79,7 +88,7 @@ class BootLoader():
 
         if self.cloud is not None and self.hardware is not None:
             print("Boot Finish")
-            self.hardware.write(text="BOOT FINISH")
+            self.hardware.write(text="Boot FINISH",led=[0,0,0],servo=[90,90,90])
 
         time.sleep(5)
         return 0
@@ -88,6 +97,8 @@ class BootLoader():
     def load_hardware(self):
         from Core.Hardware import v1
         self.hardware = v1.HardwareV1()
+        self.hardware.write(text="BootUp",led=[50,50,50],servo=[50,50,30])
+
 
         # TODO
         """
@@ -97,6 +108,7 @@ class BootLoader():
         except:
             print("[E10] hardware ERROR")
         """
+
         return 0
 
     def load_config(self):
@@ -138,6 +150,3 @@ def test():
     d = BootLoader()
     d.open()
     print("tested")
-
-
-test()
