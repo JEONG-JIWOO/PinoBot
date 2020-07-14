900c8d8f385d645ffefae8d308e69739cc84f05e
jiwoo <jiwoo@gepetto.io>
2020-07-14 22:00:05 +0900

    Refactorying.
    1. Few Feature Works
    2. ALL Feature Works
    3. No Error in Valid Input Test
    4. Multi Device Test
    
    TO-DO
    
    1. No Error in Invalid Input Test
    2. Error Handle on raise(error)
    3. invalid Init Test

--- Core/Hardware/I2C/oled.py
+++ Core/Hardware/I2C/oled.py
@@ -1,31 +1,40 @@
-from board import SCL, SDA
-import busio
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import adafruit_ssd1306
+import time
 
 # pip3 install adafruit-circuitpython-ssd1306
 # https://github.com/adafruit/Adafruit_CircuitPython_SSD1306
 
 class OLED():
-    def __init__(self,port="COM0",baud_rate = 115200):
-
+    def __init__(self,i2c, font_name = "NanumBarunGothicBold.ttf"):
+        # 0. Argument
+        self.font_path ="/home/pi/Desktop/PinoBot/Core/Hardware/Fonts/"+font_name
+        self.i2c = i2c
 
         # 1. Static Variables
-        self.port = port
-        self.baud = baud_rate
+        self.oled_size = (128, 64)
 
         # 2. variables
         self.last_reset_time = 0
         self.last_exception = ""
 
         # 3. Objects
-        self.serial = None
+        self.oled = None
+        self.unicode_font = None
+        self.consol_font = None
+        self.im = None
 
         # 4. Init Functions
         self.reset()
 
     def __del__(self):
-        self.serial.close()
-        del self.serial
+        try:
+            del self.oled  # Deconstruct Object
+            del self.unicode_font
+        except:
+            pass
 
     def reset(self):
         # 1. check last reset time,
@@ -34,30 +43,219 @@ class OLED():
             return 0
 
         # 2. if self.serial exists..
-        if self.serial is not None:
-            self.serial.close()  # Close serial
-            time.sleep(0.01)     # Wait a moment
-            del self.serial      # Deconstruct serial Object
+        if self.oled is not None:
+            try:
+                del self.oled     # Deconstruct Object
+                del self.unicode_font
+            except:
+                pass
 
         # 3. refresh last reset time
         self.last_reset_time = time.time()
 
         # 4. re open Serial
         try:
-            self.serial = serial.Serial(self.port, self.baud, timeout=0)
+            self.oled = adafruit_ssd1306.SSD1306_I2C(self.oled_size[0], self.oled_size[1], self.i2c)
+            self.oled.fill(0)
+            self.oled.show()
+            self.load_font()
         except Exception as E:
-            self.last_exception = str(E)
+            self.last_exception = "OLED.reset(), No IOError " + repr(E)
             return -1
 
-    def write(self,data):
-        # 1. try to send
+    def load_font(self):
+        # 1. try to load font from config file
         try :
-            self.serial.write(str(data))
-        # 2. Fail to send data
+            self.unicode_font = ImageFont.truetype(self.font_path, 30)
+            self.consol_font = ImageFont.truetype(self.font_path, 14)
+        except IOError :
+            print("fail to load font, load [NanumBarunGothicBold] ")
+        except Exception as E:
+            self.last_exception = "OLED.load_font(), No IOError " + repr(E)
+            return -2
+        else :
+            return 0
+
+        # 2. try to load font from pinobot Default Font
+        try :
+            self.unicode_font = ImageFont.truetype(
+                                "/home/pi/Desktop/PinoBot/Core/Hardware/Fonts/NanumBarunGothicBold.ttf", 30)
+            self.consol_font = ImageFont.truetype(
+                "/home/pi/Desktop/PinoBot/Core/Hardware/Fonts/NanumBarunGothicBold.ttf", 14)
+        except IOError:
+            print("fail to load font, load [system default] ")
+        except Exception as E:
+            self.last_exception = "OLED.load_font(), No IOError " + repr(E)
+            return -2
+        else:
+            return 0
+
+        # 3. try to load font from System Default Font
+        try:
+            self.unicode_font = ImageFont.load_default()
+        except Exception as E:
+            self.last_exception = "OLED.load_font(), No IOError " + repr(E)
+            return -2
+        else:
+            return 0
+
+    def send_image(self,image_name):
+        # 1. load image
+        r = self._load_image(image_name)
+        if r != 0:
+            return r
+        # 2. try to send
+        try :
+            self.oled.image(self.im)
+            self.oled.show()
+        # 3. Fail to send data
         except Exception as E :
             self.last_exception = str(E)
             self.reset()
             return -1
-        # 3. Success to send data
+        # 4. Success to send data
         else :
             return 0
+
+    def send_text(self,text):
+        # 1. make image from text
+        try :
+            self._text_2_image(text)
+        except Exception as E:
+            self.last_exception = "OLED.text_2_image()" + repr(E)
+            return -1
+        # 2. try to send
+        try :
+            self.oled.image(self.im)
+            self.oled.show()
+        # 3. Fail to send data
+        except Exception as E :
+            self.last_exception = str(E)
+            self.reset()
+            return -1
+        # 4. Success to send data
+        else :
+            return 0
+
+    def send_console(self, step, console_msgs):
+        # 3. make new image by font size
+        im = Image.new("L", (128,64), (0))
+        draw = ImageDraw.Draw(im)
+
+        # 4. draw text to image
+        draw.text((18, 0), console_msgs, font=self.consol_font, fill=(255), spacing=2,align="left")
+        if step > 16:
+            step = 16
+        for i in range(step):
+            self._draw_line(draw, i)
+
+        # 5. change to binary image and save it.
+        self.im = im.convert("1")
+        try :
+            self.oled.image(self.im)
+            self.oled.show()
+        # 3. Fail to send data
+        except Exception as E :
+            self.last_exception = str(E)
+            self.reset()
+            return -1
+        # 4. Success to send data
+        else :
+            return 0
+
+    def _load_image(self, image_name):
+        try :
+            path = "/home/pi/Desktop/PinoBot/media/image/"+image_name
+            image = Image.open(path)
+            im = image.resize(self.oled_size)
+            # 2. change to binary image and save it.
+            self.im = im.convert("1")
+        except FileNotFoundError as E:
+            self.last_exception = "OLED.load_image() " + repr(E)
+            return -2
+        except Exception as E :
+            self.last_exception = "OLED.load_image() " + repr(E)
+            return -1
+
+    def _text_2_image(self,unicode_text,resize = (128,64)):
+        # 1. split text by newline
+        splited_text = unicode_text.split("\n")
+
+        # 2. calculate text area
+        max_text_weight = 0  # find MAX weight of lines
+        max_text_height = 0  # find MAX height of lines
+        for line in splited_text:
+            size = self.unicode_font.getsize(line)
+            if size[0] > max_text_weight:
+                max_text_weight = size[0]
+            if size[1] > max_text_height:
+                max_text_height = size[1]
+        space = 10
+        new_line_cnt = unicode_text.count("\n") + 1
+        Image_Size = [max_text_weight, max_text_height + space]
+
+        # 3. make new image by font size
+        im = Image.new("L", (Image_Size[0], Image_Size[1] * new_line_cnt), (0))
+        draw = ImageDraw.Draw(im)
+
+        # 4. draw text to image
+        draw.text((0, 0), unicode_text, font=self.unicode_font, fill=(255), spacing=space, align="center")
+
+        # 5. change to binary image and save it.
+        im = im.resize(resize,resample=Image.LANCZOS)
+        self.im = im.convert("1")
+        return 0
+
+    def loading_scean(self ,step = 0, persent = 0, msg = ""):
+        progress_im = Image.new("L", self.oled_size, (0))
+        draw = ImageDraw.Draw(progress_im)
+
+        if step == 0 :
+            now_step = int(persent * 0.16)
+            for i in range(now_step):
+                self._draw_line(draw, i)
+
+        elif step > 0:
+            if step > 16:
+                step = 16
+            for i in range(step):
+                self._draw_line(draw, i)
+
+        if msg != "":
+            self._text_2_image(msg,resize=(110,64))
+            progress_im.paste(self.im,(18,0))
+
+        self.im = progress_im.convert("1")
+        self.oled.image(self.im)
+        self.oled.show()
+
+    def _draw_line(self,draw,step):
+        length = 10
+        y0 = 4 * (15 - step) + 3
+        y1 = y0
+        x0 = 1
+        x1 = length - x0
+        draw.line(xy=[(x0, y0), (x1, y1)], fill=(255), width=3)
+
+
+def test():
+    i2c = board.I2C()
+    A = OLED(i2c)
+    #A.send_text(" 힘쎄고 좋은아침! \n 내이름을 묻는다면 \n 나는 왈도 ")
+    #A.send_image("asdf.jpg")
+    print(A.last_exception)
+
+    for i in range(15):
+        A.loading_scean(persent=7*i,msg ="로딩중.. \n 대기")
+        time.sleep(0.5)
+    A.loading_scean(100)
+    time.sleep(1)
+    A.loading_scean()
+
+    for i in range(15):
+        A.loading_scean(step=i)
+        time.sleep(0.5)
+    A.loading_scean(16)
+
+if __name__ == '__main__':
+    test()
--- Core/Hardware/v1.py
+++ Core/Hardware/v1.py
@@ -17,47 +17,99 @@ I2C-1 : Address[] : PCA9685 PWM MODULE
 I2C-1 : Address[] : character LCD module
 
 """
-import logging
+import logging , time
+import busio
+import board
 
-from Hardware.I2C import servo
-from Hardware.SPI import apa_102
-from Hardware import PinoGPIO , PinoUart
+from Core.Hardware.I2C import servo , oled
+from Core.Hardware import pino_gpio
+from Core.Hardware.SPI import rgb_led
 
 #from queue import Queue
 # spi, i2c raspi-config
 
 class HardwareV1():
-    def __init__(self):
+    def __init__(self,ini):
+        # 0. Argument
+        self.ini = ini
+
         # 1. Static Variables
 
         # 2. variables
+        self.last_reset_time = 0
+        self.last_exception = ""
 
         # 3. Objects
+        self.I2C_BUS = None
+        self.SERVO = None
+        self.OLED = None
+        self.SPI_BUS = None
+        self.RGB_LED = None
+        self.SENSOR = None
+        self.log = None
 
-        # 3.1 logs
-        self.log = self.set_logger()
+        # 4. Init Functions
+        self.reset()
+        self._set_default()
 
-        self.SERVO = servo.SERVO()
-
-        self.LED = apa_102.APA102(num_led=2)
-
-        self.GPIO = PinoGPIO.Pino_GPIO()
-
-        self.UART = PinoUart.Pino_UART(port= "COM0", baud_rate = 115200)
-
-        self.setLED(0,[0,0,0])
-        self.setLED(1,[0,0,0])
 
     def __del__(self):
-        self.setLED(0,[0,0,0])
-        self.setLED(1,[0,0,0])
-        self.LED.cleanup()
+        for object in [ self.SERVO,self.OLED ,self.RGB_LED ,self.SENSOR, self.I2C_BUS,self.SPI_BUS]:
+            if object is not None:
+                try:
+                    del object
+                except:
+                    pass
 
-    def setLED(self, i, colors):
-        self.LED.set_pixel(i, int(colors[0]), int(colors[1]), int(colors[2]))
-        self.LED.show()
-    
+    def reset(self):
+        # 1. check last reset time,
+        #    only can reset after 1min after last reset
+        if (time.time() - self.last_reset_time) < 60:
+            return 0
 
+        # 2. if object exists..
+        for object in [ self.SERVO,self.OLED ,self.RGB_LED ,self.SENSOR, self.I2C_BUS,self.SPI_BUS]:
+            if object is not None:
+                try:
+                    del object
+                except:
+                    pass
+
+        # 3. refresh last reset time
+        self.last_reset_time = time.time()
+
+        # 4. re open GPIO
+        try:
+            # 4.1 init I2C
+            from board import SCL, SDA
+            self.I2C_BUS = busio.I2C(SCL, SDA)
+            self.OLED = oled.OLED(self.I2C_BUS)
+            self.OLED.send_console(1,"Hardware init...   ")
+
+            self.SERVO = servo.SERVO(self.I2C_BUS)
+            time.sleep(0.3)
+            # 4.1 init SPI
+            self.SPI_BUS = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+            self.RGB_LED = rgb_led.RGB_LED(self.SPI_BUS)
+
+            # 4.1 init GPIO
+            self.SENSOR = pino_gpio.Pino_GPIO()
+            self.OLED.send_console(2,"Hardware init...  \n" +
+                                     "Hardware reset..    ")
+
+            self.log = self.set_logger()
+            time.sleep(0.3)
+
+        except Exception as E:
+            self.last_exception = "reset() ," + repr(E)
+            return -1
+
+    def _set_default(self):
+        self.OLED.send_console(3,"Hardware init...   \n"+
+                                 "Hardware reset..   \n"+
+                                 "Hardware Done!..   ")
+        self.RGB_LED.write([100,50,50,50,100,50])
+        self.SERVO.write([0,0,0,0,0],1)
 
     """
     A.1 Initializing Logger
@@ -69,7 +121,7 @@ class HardwareV1():
         formatter = logging.Formatter('[%(levelname)s] (%(asctime)s : %(filename)s:%(lineno)d) > %(message)s')
 
         # 2.2 set file logger 
-        log_file = logging.FileHandler(filename = '/home/pi/Desktop/PinoBot/log/HardwareV1.log',
+        log_file = logging.FileHandler(filename = '/home/pi/Desktop/PinoBot/log/HardwareV11.log',
                                             mode='w',
                                             encoding='utf-8')
         log_file.setFormatter(formatter)
@@ -84,31 +136,48 @@ class HardwareV1():
         log.info("Start Hardware Module")
         return log
 
-    def write(self, text ="", led = [], servo = []):
-        
-        if text != "":
-            #self.log.info("LCD SHOW text %s"%text )
-            self.LCD.send_msg(text)
-        
-        if len(led) == 3:
-            self.setLED(0,led)
-            self.setLED(1,led)
+    def write(self, image = None, text =None, led = None, servo_angle = None, servo_time = None):
 
-        if len(led) == 6:
-            self.setLED(0,led[0:3])
-            self.setLED(1,led[3:6])
+        # 1. send image
+        if image is not None:
+            result = self.OLED.send_image(image)
+            if result == -1 and self.log is not None :
+                self.log.error(self.OLED.last_exception)
+                self.OLED.last_exception = ""
 
-        if len(servo) == 2:
-            self.SERVO.send_angles(servo[0],servo[1])
+        # 2. send text
+        if text is not None:
+            result = self.OLED.send_text(text)
+            if result == -1 and self.log is not None :
+                self.log.error(self.OLED.last_exception)
+                self.OLED.last_exception = ""
 
-        elif len(servo) == 3:
-            self.SERVO.send_angles(8 ,servo[0])
-            self.SERVO.send_angles(9, servo[1])
-            self.SERVO.send_angles(10, servo[2])
+        # send led
+        if led is not None:
+            result = self.RGB_LED.write(led)
+            if result == -1 and self.log is not None :
+                self.log.error(self.RGB_LED.last_exception)
+                self.RGB_LED.last_exception = ""
 
-    def read_sw(self):
-        return (self.SW.read_once())
-    
-    def read_sonic(self):
-        return (self.SonicSensor.measure_once())
-        
+        # send servo
+        if servo_angle is not None:
+            result = self.SERVO.write(servo_angle,servo_time)
+            if result == -1 and self.log is not None :
+                self.log.error(self.SERVO.last_exception)
+                self.SERVO.last_exception = ""
+
+        # check Error Logs
+        if self.log is not None :
+            for comp in [self.OLED,self.RGB_LED,self.SERVO, self.SENSOR]:
+                if len(comp.last_exception) :
+                    self.log.error(comp.last_exception)
+                    comp.last_exception = ""
+
+def test():
+    A = HardwareV1("")
+    A.write(text="하이루",led=[0,0,100],servo_angle=[180,180,90],servo_time=2)
+    A.write(image="asdf.jpg")
+    A.write(text="아아")
+
+if __name__ == '__main__':
+    test()
