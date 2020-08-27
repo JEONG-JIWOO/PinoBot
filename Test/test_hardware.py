0f91fa76e7ed28f391f2dd4d507963ebff2d1c70
jiwoo <jiwoo@gepetto.io>
2020-08-27 17:55:52 +0900

    function order change

--- Test/test_hardware.py
+++ Test/test_hardware.py
@@ -6,23 +6,6 @@ Core.Hardware.SPI.Pino_LED
 
 import unittest
 
-
-class CustomTests(unittest.TestCase):
-    def setUp(self):
-        """ 테스트 시작전 테스트 설정"""
-        pass
-
-    def tearDown(self):
-        """ 테스트 끝난이후 실행되는 함수"""
-        pass
-
-    def test_runs(self):
-        """ 실 테스트 코드 """
-        custom_function()
-
-if __name__ == '__main__':
-    unittest.main()
-
 def custom_function():
     """
     Module TEST codes
@@ -49,6 +32,22 @@ def custom_function():
     hardware.write(servo_angle=1)
     hardware.write(serial_msg=1)
     print(hardware.read())
+
+class CustomTests(unittest.TestCase):
+    def setUp(self):
+        """ 테스트 시작전 테스트 설정"""
+        pass
+
+    def tearDown(self):
+        """ 테스트 끝난이후 실행되는 함수"""
+        pass
+
+    def test_runs(self):
+        """ 실 테스트 코드 """
+        custom_function()
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
