87b5b543305e403f2f9a978cf0d046065a66d9d5
jiwoo <jiwoo@gepetto.io>
2020-08-27 17:55:48 +0900

    function order change

--- Test/test_i2c.py
+++ Test/test_i2c.py
@@ -6,24 +6,6 @@ Core.Hardware.SPI.Pino_LED
 
 import unittest
 
-
-class CustomTests(unittest.TestCase):
-    def setUp(self):
-        """ 테스트 시작전 테스트 설정"""
-        pass
-
-    def tearDown(self):
-        """ 테스트 끝난이후 실행되는 함수"""
-        pass
-
-    def test_runs(self):
-        """ 실 테스트 코드 """
-        custom_function_1()
-        custom_function_2()
-
-if __name__ == '__main__':
-    unittest.main()
-
 def custom_function_1():
     import board ,time
     from Core.Hardware.I2C import pino_oled
@@ -104,3 +86,22 @@ def custom_function_2():
     servo_board.write([180], 2)
     servo_board.write([1], 2)
     return True
+
+
+class CustomTests(unittest.TestCase):
+    def setUp(self):
+        """ 테스트 시작전 테스트 설정"""
+        pass
+
+    def tearDown(self):
+        """ 테스트 끝난이후 실행되는 함수"""
+        pass
+
+    def test_runs(self):
+        """ 실 테스트 코드 """
+        custom_function_1()
+        custom_function_2()
+
+if __name__ == '__main__':
+    unittest.main()
+
--- Test/test_main.py
+++ Test/test_main.py
@@ -7,6 +7,30 @@ Core.Hardware.SPI.Pino_LED
 import unittest
 
 
+def event_test():
+    from Core.pino_main import PinoBot
+    a = PinoBot()
+
+    # 5. fail handler False
+    a.run_task(["event", "Fail_NoMatch_Intents", None, False])
+
+    # 1. valid
+    a.run_task(["event","Fail_NoMatch_Intent",None,True]) #  FailNoMatchIntent
+
+    p = {'a':'asdf'}
+    # 2. add not needed parameter
+    a.run_task(["event", "Fail_NoMatch_Intent",p, True])  #  FailNoMatchIntent
+
+    # 3. invalid event name
+    a.run_task(["event", "Fail_NoMatch_Intents",None, True]) # FailNoMatchIntent
+
+    # 4. invalid event name & parameter
+    a.run_task(["event", "Fail_NoMatch_Intents",p, True]) # FailNoMatchIntent
+
+    # 5. fail handler False
+    a.run_task(["event", "Fail_NoMatch_Intents", None, False])
+
+
 class CustomTests(unittest.TestCase):
     def setUp(self):
         """ 테스트 시작전 테스트 설정"""
@@ -18,7 +42,7 @@ class CustomTests(unittest.TestCase):
 
     def test_runs(self):
         """ 실 테스트 코드 """
-        custom_function()
+        event_test()
 
 if __name__ == '__main__':
     unittest.main()
