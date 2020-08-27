df0bfec3642aa042f3b0e811e8078ef3880f08bb
jiwoo <jiwoo@gepetto.io>
2020-08-27 17:58:21 +0900

    sh -> bash, change repo

--- Install/Install.sh
+++ Install/Install.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
-# http://wiki.seeedstudio.com/ReSpeaker_4_Mic_Array_for_Raspberry_Pi/
+printf   "\n\n [step 1], update & upgrade rpi \n\n"
 
-echo -e "/n/n [step 1.1], update & upgrade rpi /n/n"
+#apt-get update
+#apt-get upgrade
+python3 -m pip install --upgrade setuptools
 
-sudo apt-get update
-sudo apt-get upgrade
+printf   "\n\n [step 2], Install Driver"
 
-echo -e "/n/n [step 1.2], download driver source"
-
-git clone https://github.com/respeaker/seeed-voicecard
-
-echo -e "/n/n [step 1.3], Install"
-
-sudo ./seeed-voicecard/install.sh
-
-
-echo -e "/n/n [step 2.1], install portAudio19 /n/n"
+git clone https://github.com/JEONG-JIWOO/seeed-voicecard
+sudo bash ./seeed-voicecard/install.sh --compat-kernel
+apt-get install portaudio19-dev
 
 # port audio 설치 가이드라인인, 아래 패키지 설치하면 리스피커 모듈과 충돌.
-#sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 
-sudo apt-get install portaudio19-dev
+#apt-get install libportaudio0 libportaudio2 libportaudiocpp0
 
-echo -e "/n/n [step 2.2], install python3-dev /n/n"
-sudo apt-get install python3-dev
+printf   "\n\n [step 4], install python3-dev \n\n"
+apt-get install python3-dev
 
-echo -e "/n/n [step 2.3], install pyaudio /n/n"
+printf   "\n\n [step 5], install pyaudio \n\n"
 pip3 install pyaudio
+pip3 install Pillow
 
-echo -e "/n/n [step 3], install dialogflow /n/n"
-python3 -m pip install --upgrade setuptools
+printf   "\n\n [step 6], install dialogflow \n\n"
 python3 -m pip install -r requirements.txt
 pip3 install dialogflow
 
-echo -e "/n/n [step 4], reboot in 2 seconds"
-sleep 1
-echo -e "/n/n [step 4], reboot in 1 seconds"
-sleep 1
+printf   "\n\n [step 7], install Hardware \n\n"
+pip3 install RPi.GPIO
+pip3 install adafruit-circuitpython-busdevice
+pip3 install Adafruit-circuitpython-SSD1306
+pip3 install adafruit-circuitpython-pca9685
+pip3 install adafruit-circuitpython-motor
+
+for i in {10..0}
+do
+  printf   "\n [step 8], reboot in $i seconds"
+  sleep 1
+done
 
 reboot
