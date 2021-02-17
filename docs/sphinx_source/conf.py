8206fc9fc57f3127f26007f34756afcaf7e73618
Front-line-dev <kimminsung@kakao.com>
2021-02-18 00:23:27 +0900

    feat: add lightbox attribute to images

--- docs/sphinx_source/conf.py
+++ docs/sphinx_source/conf.py
@@ -29,13 +29,9 @@ release = '1.0.00'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "m2r2",
-    "sphinxcontrib.images"
+    "m2r2"
 ]
 source_suffix = ['.rst', '.md']
-images_config = {
-    'override_image_directive': False
-}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
