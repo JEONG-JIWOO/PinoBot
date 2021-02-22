d0d596c775ca4e41356748025093871128e1a04e
jiwoo <jiwoo@gepetto.io>
2021-02-22 15:16:54 +0900

    add chmod command to log file

--- PinoRun.sh
+++ PinoRun.sh
@@ -5,4 +5,5 @@ cp -r -u /boot/PinoSet/media /home/pi/Desktop/PinoBot || echo " skip copy from /
 chmod 777 -R /home/pi/Desktop/PinoBot/PinoConfig.ini || echo " skip chmod [1]"
 chmod 777 -R /home/pi/Desktop/PinoBot/keys || echo " skip skip chmod [2]"
 chmod 777 -R /home/pi/Desktop/PinoBot/media || echo " skip skip chmod [3]"
+chmod 777  /home/pi/Desktop/PinoBot/main.log || echo " skip skip chmod [4]"
 /usr/bin/python3 -u /home/pi/Desktop/PinoBot/pino_main.py &&echo " Launch PinoBot"
