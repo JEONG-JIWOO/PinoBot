7d725c16e4f08f6c90dcaa630c4eaf3067f29a06
jiwoo <jiwoo@gepetto.io>
2021-03-08 21:13:09 +0900

    remove pinoset

--- PinoRun.sh
+++ PinoRun.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-cp -r -u /boot/PinoSet/PinoConfig.ini /home/pi/Desktop/PinoBot/PinoConfig.ini  || echo " skip copy from /boot [1]"
-cp -r -u /boot/PinoSet/keys /home/pi/Desktop/PinoBot || echo " skip copy from /boot [2]"
-cp -r -u /boot/PinoSet/media /home/pi/Desktop/PinoBot || echo " skip copy from /boot [3]"
 chmod 777 -R /home/pi/Desktop/PinoBot/PinoConfig.ini || echo " skip chmod [1]"
 chmod 777 -R /home/pi/Desktop/PinoBot/keys || echo " skip skip chmod [2]"
 chmod 777 -R /home/pi/Desktop/PinoBot/media || echo " skip skip chmod [3]"
