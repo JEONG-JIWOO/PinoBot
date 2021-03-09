f99063515448ec5958805892f077edca6b0cc038
Front-line-dev <kimminsung@kakao.com>
2021-03-09 22:29:15 +0900

    Create jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.open_browser = False
+# password=pinobot
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$ruGoXAH8ZdE/AVVvwEreuQ$olnDp/EZqk3D0dj0CuCSpg'
+c.NotebookApp.password_required = True
+c.NotebookApp.port = 8080
