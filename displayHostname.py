195392a09387e903271c5c6a54a16d93a5d6c0b5
jiwoo <jiwoo@gepetto.io>
2021-03-17 17:00:54 +0900

    기능추가 5개
    1. ip주소 띄우는 기능추가
    2. 최대 5회까지 인터넷 연결을 확인하면서 (각 루프당 최소 3.5초의 지연시간포함), 인터넷 연결되지 않앗을경우 internet check..를 연결되면 ip및 호스트 이름 디스플레이
    3. 5회 시도 실패시 인터넷 에러 메세지 디스플레이
    4.  displayHostname.py 병렬실행 추가

--- displayHostname.py
+++ displayHostname.py
@@ -1,9 +1,37 @@
 import board
 from modules.Hardware.I2C.pino_oled import Pino_OLED
-import socket
+import socket , time
+from subprocess import check_output
 
 if __name__ == "__main__":
+
     I2C_BUS = board.I2C()
     oled = Pino_OLED(I2C_BUS, "/home/pi/Desktop/PinoBot/")
-    hostname = socket.gethostname()
-    oled.send_text(hostname + '/')
+
+    connected = False
+    for i in range(5):
+        try:
+            oled.send_text("internet \n check"+i*".")
+            time.sleep(0.5)
+            from urllib3 import PoolManager, Timeout, Retry
+
+            http = PoolManager(
+                timeout=Timeout(connect=3.0, read=2.0), retries=Retry(2, redirect=0)
+            )
+            response = http.request("HEAD", "https://status.cloud.google.com/")
+
+        except Exception as E:
+            time.sleep(3)
+            oled.send_text("internet \n wait."+i*".")
+
+        else :
+            hostname = socket.gethostname()
+            ip = check_output(['hostname', '-I']).decode().strip()
+            print("internet on \nhostname :", hostname)
+            print("ip :", hostname)
+            oled.send_text(ip + "\n " + hostname)
+            connected = True
+            break
+
+    if not connected :
+        oled.send_text("internet \n error "+i*".")
