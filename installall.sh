46ccd86918894d47743fe5176aae30974d22d5b7
jiwoo <jiwoo@gepetto.io>
2021-03-17 16:14:52 +0900

    bash 단어추가
    사운드카드 드라이버가 install.sh 만 돌렸을때는 설치가 안되는걸로 보이는데, bash가 빠져서 그런듯

--- installall.sh
+++ installall.sh
@@ -44,7 +44,7 @@ yes | pip3 install --upgrade setuptools
 printf   "\n\n [step 2], Install Driver"
 git clone https://github.com/HinTak/seeed-voicecard #HinTak's repository is more stable than official
 cd ./seeed-voicecard || { prirntf "cd seeed-voicecard fail "; exit 127;}
-sudo ./install.sh || { prirntf "\n\n cd seeed-voicecard install fail , try install use --compat-kernel" ; sudo bash ./install.sh --compat-kernel ;}
+sudo bash ./install.sh || { prirntf "\n\n cd seeed-voicecard install fail , try install use --compat-kernel" ; sudo bash ./install.sh --compat-kernel ;}
 cd ..
 rm -rf ./seeed-voicecard || { prirntf "remove seeed-voicecard fail "; exit 127;}
 
