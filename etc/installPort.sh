138795ebc593685ee9580dfdc2fb2e7eeecb2221
Front-line-dev <kimminsung@kakao.com>
2021-03-24 20:43:10 +0900

    feat: merge port redirect service script

--- etc/installPort.sh
+++
@@ -1,6 +0,0 @@
-# add service
-
-cd /home/pi/Desktop/PinoBot
-cp ./etc/redirectPort.service /etc/systemd/system/redirectPort.service
-systemctl enable redirectPort.service
-systemctl start redirectPort.service
